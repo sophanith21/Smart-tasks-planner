4892116dc8376b410272e417bccfeec71f282f5f
Meas Sophanith <sophanithmeas91@gmail.com>
2025-07-27 13:49:43 +0700

    Fix logo name

--- Client/src/pages/Landing.jsx
+++ Client/src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/Logo.jpg";
+import logo from "../assets/smart_tasks_panner.jpg";
 import task from "../assets/task.svg";
 import robot from "../assets/AI.svg";
 import history from "../assets/history.svg";
--- Client/src/pages/components/Header.jsx
+++ Client/src/pages/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "../../assets/Logo.jpg";
+import logo from "../../assets/smart_tasks_panner.jpg";
 import { getAuthUser, logout } from "../../utils/auth";
 import { useContext } from "react";
 import { AuthContext } from "../../context/authContext.jsx";
